70c443fb680a69066ca31e74aa260f90e332d7c1
Rhythm <rhythmrv3@gmail.com>
2023-03-26 18:10:47 +0530

    Refactoring Card and Bucket Component

--- src/components/Bucket.jsx
+++ src/components/Bucket.jsx
@@ -3,9 +3,18 @@ import { Link } from 'react-router-dom';
 
 function Bucket(props){
     return (
-        <div>
+      <div class="m-2 w-full md:w-1/2 xl:w-1/3 p-6 w-[19.875rem] rounded-lg bg-white text-[0.8125rem] leading-5 text-slate-900 shadow-xl shadow-black/5 ring-1 ring-slate-700/10">
+          <div class="flex items-center p-4 pb-0">
+          <div class="ml-4 flex-auto">
+          <div class="text-lg flex justify-center">
           <Link to={`/bucket/${props.id}`}>{props.name}</Link>
-        </div>
+          </div>
+          </div>
+          </div>
+          <div class="flex gap-3 p-4 flex justify-center">
+          <div class="pointer-events-auto rounded-md bg-indigo-600 py-2 px-3 text-[0.8125rem] font-semibold leading-5 text-white hover:bg-indigo-500"><Link to={`/bucket/${props.id}`}>Show Bucket</Link></div>
+          <div class="pointer-events-auto rounded-md py-2 px-4 text-center font-medium shadow-sm ring-1 ring-slate-700/10 hover:bg-slate-50">Delete Bucket</div></div>
+      </div>
       );
 }
 
--- src/components/BucketPage.jsx
+++ src/components/BucketPage.jsx
@@ -59,19 +59,7 @@ function loadCards(cards){
     
     return cards.map((card,index)=>{
         return(
-            <div class="m-2 w-full md:w-1/2 xl:w-1/3 p-6 w-[19.875rem] rounded-lg bg-white text-[0.8125rem] leading-5 text-slate-900 shadow-xl shadow-black/5 ring-1 ring-slate-700/10">
-                <div class="flex items-center p-4 pb-0">
-                <div class="ml-4 flex-auto">
-                <div class="text-lg flex justify-center">
-                <Card key={index} title={card.title}/>;
-                </div>
-                </div>
-                </div>
-                <div class="flex gap-3 p-4 flex justify-center">
-                <div class="pointer-events-auto rounded-md bg-indigo-600 py-2 px-3 text-[0.8125rem] font-semibold leading-5 text-white hover:bg-indigo-500">Delete card</div>
-                <div class="pointer-events-auto rounded-md py-2 px-4 text-center font-medium shadow-sm ring-1 ring-slate-700/10 hover:bg-slate-50">Delete Bucket</div></div>
-
-            </div>
+          <Card key={index} title={card.title}/>
         )
     })
 }
